7b4540663b173a28194f7716de2a9ff7b074b97e
Michele <pianomaik@gmail.com>
2022-02-18 18:21:20 +0100

    added banner.txt

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+    @Override
+    public String getJoke() {
+        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
